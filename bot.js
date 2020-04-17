a91a5c659910da06b90250fd0e5395e7df487eba
Glitch (pumped-chopper) <none>
2020-04-17 09:31:57 +0000

    💘🎷 Checkpoint ./index.js:566927/1801 ./.gitignore:566927/1 ./package.json:566927/47 ./bot.js:566927/546

---
+++ bot.js
@@ -0,0 +1,15 @@
+const discord = require('discord.js')
+const client = new discord.Client()
+
+client.on('ready', message => {
+  console.log(`${client.user.tag}にログインしました！`);
+})
+
+client.on('message', message => {
+  if (message.mentions.users.has(client.user.id)) {
+    message.reply('呼びましたか？')
+    return
+  }
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // Response for Uptime Robot
-const http = require("http");
+const http = require('http')
 http
   .createServer(function(request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Discord bot is active now \n");
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.end('Discord bot is active now \n')
   })
-  .listen(3000);
+  .listen(3000)
 
 // Discord bot implements
-const discord = require("discord.js");
-const client = new discord.Client();
+const discord = require('discord.js')
+const client = new discord.Client()
 
-client.on("ready", message => {
-  console.log("bot is ready!");
-});
+client.on('ready', message => {
+  console.log(`${client.user.tag}にログインしました！`);
+})
 
-client.on("message", message => {
+client.on('message', message => {
   if (message.mentions.users.has(client.user.id)) {
-    message.reply("呼びましたか？");
-    return;
+    message.reply('呼びましたか？')
+    return
   }
-});
+})
 
 if (process.env.DISCORD_BOT_TOKEN == undefined) {
-  console.log("please set ENV: DISCORD_BOT_TOKEN");
-  process.exit(0);
+  console.log('tokenが設定されていません！')
+  process.exit(0)
 }
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN)
