daf37deadc153433fbf78cb19c99c3ee9106bb72
Glitch (pumped-chopper) <none>
2020-04-17 09:43:55 +0000

    😬🐘 Checkpoint ./index.js:566927/458

--- index.js
+++ index.js
@@ -7,24 +7,9 @@ http
   })
   .listen(3000)
 
-// Discord bot implements
-const discord = require('discord.js')
-const client = new discord.Client()
-
-client.on('ready', message => {
-  console.log(`${client.user.tag}にログインしました！`);
-})
-
-client.on('message', message => {
-  if (message.mentions.users.has(client.user.id)) {
-    message.reply('呼びましたか？')
-    return
-  }
-})
-
 if (process.env.DISCORD_BOT_TOKEN == undefined) {
   console.log('tokenが設定されていません！')
   process.exit(0)
 }
 
-client.login(process.env.DISCORD_BOT_TOKEN)
+require('./bot.js')
