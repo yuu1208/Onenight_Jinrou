a6370785ff0b8e2709bf43cf0c3316bdf79230cf
Glitch (pumped-chopper) <none>
2021-01-22 19:05:10 +0000

    👵🏬 Checkpoint ./bot.js:20482770/1561

--- bot.js
+++ bot.js
@@ -1,15 +1,27 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-client.on('ready', () => {
-  console.log(`${client.user.tag}にログインしました！`)
-})
+const BOT_PREFIX = (/^おひよ/);
+const mension_message = ["どうしたの？","ん、どした？？","呼んだ〜？","はーい！"];
 
 client.on('message', message => {
-  if (message.mentions.users.has(client.user.id)) {
-    message.reply('呼びましたか？')
-    return
+  
+  //再帰呼び出し対策
+  //BOTが話した語句には応答しない。
+  if(message.author.id == client.user.id) {
+    return;
   }
+  
+  //ご挨拶
+  if(message.mentions.users.has(client.user.id) || message.content.match(BOT_PREFIX)) {
+    message.channel.send(mension_message[Math.floor(Math.random() * mension_message.length) + 0]);
+  }
+  
+  //動作
+  if(message.content.match(BOT_PREFIX) && message.content.match(_|おはよ/)) {
+    
+  }
+  
 })
 
 client.login(process.env.DISCORD_BOT_TOKEN)
