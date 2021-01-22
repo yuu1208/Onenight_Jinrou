3f8918ccb0d103c98dfeef22fed73003a01eb057
Glitch (pumped-chopper) <none>
2021-01-22 19:16:52 +0000

    🐡🎤 Checkpoint ./bot.js:20482770/2363

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+const isset = function(data){ if(data === "" || data === null || data === undefined){ return false; }else{ return true; } }; 
 const BOT_PREFIX = (/^おひよ/);
-const mension_message = ["どうしたの？","ん、どした？？","呼んだ〜？","はーい！"];
+
 
 client.on('message', message => {
   
@@ -13,13 +14,31 @@ client.on('message', message => {
   }
   
   //ご挨拶
-  if(message.mentions.users.has(client.user.id) || message.content.match(BOT_PREFIX)) {
-    message.channel.send(mension_message[Math.floor(Math.random() * mension_message.length) + 0]);
+  if(message.mentions.users.has(client.user.id)) {
+    message.channel.send("私は知的なエージェントとして、ゆうにより開発されました。”おひよ”と申します！\n何か話したい場合は、「おひよ、眠い」などと話しかけて下さい！");
+    return;
+  }
+  
+  if(message.content == "おひよ") {
+    const MSG_greeting = ["どうしたの？","ん、どした？？","呼んだ〜？","はーい！"];
+    message.channel.send(MSG_greeting[Math.floor(Math.random() * MSG_greeting.length) + 0]);
+    return;
   }
   
   //動作
-  if(message.content.match(BOT_PREFIX) && message.content.match(_|おはよ/)) {
-    
+  if(message.content.match(BOT_PREFIX) && message.content.match(/起き|おはよ/)) {
+    message.channel.send("おはよ〜" + message.member.displayName + "！");
+    let 
+    return;
+  }
+  if(message.content.match(BOT_PREFIX) && message.content.match(/眠|ねむ(く|い)/)) {
+    const MSG_SLEEPY = ['眠いときにねるのが一番だけど、やることは終わった？','寝ちゃう？','そっかおやすみ〜((','もう寝ちゃうんですか...?'];
+    message.channel.send(MSG_SLEEPY[Math.floor(Math.random() * MSG_SLEEPY.length) + 0]);
+    return;
+  }
+  
+  if(message.content.match(BOT_PREFIX) && isset(message.content)) {
+    message.channel.send("ごめん、私にはわからない、、");
   }
   
 })
