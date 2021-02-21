f368793fd98da106ba0bad2d292980afa155fbd8
Glitch (pumped-chopper) <none>
2021-02-21 19:09:42 +0000

    😻⛴ Checkpoint ./bot.js:20482770/7318

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 require('./GAME_jinrou.js');
 //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 const youbi = ["日","月","火","水","木","金","土"];
+const omikuji = ["🔴 大吉","🟡 中吉","🟢 吉","🟣 小吉","🔵 凶"];
 const EEW_icon = "https://play-lh.googleusercontent.com/91udG1PTmvsm_u7dvQmj4MuW3KK4-vQjXl359qnHkXDYH_wxKkdE6Fo6pBI6zngsqxw";
 const EEW_MSG = [":warning::loudspeaker: **緊急地震速報（気象庁発表）**","緊急地震速報を受信しました。強い揺れに備えてください。\n地震の詳細の情報は以下のとおりです。\n\n","\n⚠**一律配信のため、一部地域では揺れない場合があります**\n\nこの情報を鵜呑みにせず、テレビ・ラジオ等の電源をONにし最新情報を得て下さい。\n\nまた、すでに揺れている場合は、落ち着いて行動・避難してください。\n\n津波の恐れがありますので、海岸や河口の近くにいる方は、直ちに安全な高台や津波避難ビルに避難してください。 \n\n> NHKニュース（災害時地上波同時配信）\nhttps://plus.nhk.jp/watch/ch/g1\n\n> Yahooリアルタイム地震モニター\nhttps://typhoon.yahoo.co.jp/weather/jp/earthquake/kyoshin```\n\n @everyone"];
 const EEW_QUAKE = ["４以上","５弱以上（最大震度上昇）","５強以上（最大震度上昇）","6弱以上（最大震度上昇）","６強以上（最大震度上昇）","７（最大震度上昇）"];
@@ -17,6 +18,7 @@ const Punish_Pri = /^0[789]0-\d{4}-\d{4}$|^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.
 let LEFT_REASON = "自主的に";
 var judge = "正常";
 var reason;
+let suffix;
 let eew_cnt = 0;
 //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 const roles = ["800742672947871749","800742672947871750","800742672947871748","800742672947871747","800742672947871746"];
@@ -161,73 +163,44 @@ client.on('message', async message => {
   let FILE_WRITE = getTime_JPN() + ',' + message.member.displayName + ',' + message.member + ',' + message.channel + ',' + message.content + ',' + message.id + ',' + judge + "\n";
   fs.appendFileSync("logs/logs.csv", FILE_WRITE);
   //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+  // この下から通常処理
+  //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
   
-  
-    //年齢制限を聞くやつ
-  if (message.content == "MSG_RULE") {
-    message.channel.send(
-      {embed: {color: 0xcccccc,fields: [
-        {name: "📔 サーバールール・利用規約",value: "当サーバーに入場し、年齢確認を終えた方は、以下のルールと利用規約に同意し、いかなる処分を受けてもそれに服従すると同時に、下記に記載する事項で発生した損害等については、運営は一切の責任を追わないものとします。",inline: false},
-        {name: "▼ 年齢確認",value: "```・当サーバーのBOTは、年齢確認ができるものとします。\n・あくまでも自己申告制ですので、身分証明書などを提出する必要はありません```",inline: false},
-        {name: "▼ 禁止事項",value: "```・ハラスメント行為（セクハラ・モラハラ等）\n・チャットやVCで執拗に入退出や送信をする行為\n・相手が拒否しているのにも関わらず個人情報等を聞き出す行為\n・当サーバーやユーザーの情報を蓄積/販売する行為\n・連続でBOTが反応する語句を送信する行為\n・セキュリティの脆弱性を突く/突こうと試みる行為\n・その他、運営が不適切と判断したもの```",inline: false},
-        {name: "▼ VC使用時の注意",value: "```・ゲーム専用VCや雑談専用VCがありますので、用途に合う部屋を使用してください。\n・聞き専（ミュート勢の方など）は聞き専CHを使用してください。\n・用途以外のVCの使用は禁止です。```",inline: false},
-        {name: "当サーバーは迷惑行為検知システムにより、24時間365日監視しています",value: "万一、検知システムに反応しない迷惑行為を発見した場合には、いかなる処罰・法的措置も辞さないことを当利用規約に記します。\n\nこのサーバーを使うみなさまが、安心して利用できるコミュニティを継続するために、ルール厳守にご理解ご協力をお願い致します。",inline: false},
-      ]}});
-   return;
-  }
-  
-  else if (message.content == "MSG_WELCOME") {
-    message.channel.send(
-      {embed: {color: 0xEC407A,fields: [
-        {name: "🎉 ようこそサーバーへ！",value: "数あるサーバーの中から当サーバーを選んで頂き、ありがとうございます！",inline: false},
-        {name: "■ まずなにする？",value: "**”年齢認証にご協力下さい”**タブより、年齢確認のご協力をお願いします！\n年齢認証が完了するまで、通常のチャットやVCはご利用いただけません！",inline: false},
-        {name: "■ 年齢認証が終わったら",value: "一般チャットで初めての挨拶をしてみたり、会話に参加してみよう！\nVCに参加するのもGOOD！",inline: false},
-        {name: "■ 年齢制限後に一般チャット等が表示されない場合",value: "BOTが処理をするのに時間がかかっています。\n通常は数秒(長くても10秒)で終わりますが、もし表示されない場合は @ًゆう までご一報下さい！",inline: false},
-        {name: "■ 告知：サーバーのレビューをお願いします！",value: "サーバーに７日間以上滞在すると、DISBOARDにてサーバーのレビューが書き込めるようになります！新しいユーザーの方を勧誘するためにも、レビュー投稿に是非お願い致します！\n\nレビューはこちらから！\nhttps://disboard.org/ja/review/create/800742672947871744",inline: false},
-      ]}});
-   return;
-  }
-  
-  
-  else if (message.content == "MSG_AGECHECK") {
-    message.channel.send(
-      {embed: {color: 0xEC407A,fields: [
-        {name: "⚠ 年齢認証",value: "当サーバーでは、青少年の方の健全な育成に心がけております。利用者の方々には、13歳未満でないことの確認を行っております。なお、確認はBOTが自動的に行います。年齢はBOTのサーバーに保管されます。そのため、__運営があなたの年齢を知ることはできません。__",inline: false},
-        {name: ":pray: 年齢確認のご協力のお願い",value: "チャット欄に、年齢を __**数値のみで**__ 送信してください。\n送信して数秒経つと、送信して頂いた年齢は自動的に削除され、チャット、VCが利用できるようになります。\n\n例: 18歳の場合 ➟ 「18」と **__”半角英数”__** で送信してください。\n",inline: false}
-      ]}});
-   return;
-  }
-  
-  //ルール
-  else if (message.content == "MSG_NEWCOMER") {
-    message.channel.send(
-      {embed: {color: 0x10eb73,fields: [
-        {name: "☑ サーバールールと利用規約",value: "当サーバーに入場し、年齢確認を終えた方は、以下のルールと利用規約に同意し、いかなる処分を受けてもそれに服従すると同時に、下記に記載する事項で発生した損害等については、運営は一切の責任を追わないものとします。",inline: false},1111111111111
-      ]}});
-   return;
-  }
-  
-  
-  //警告
-  if (message.content == "MSG_WARN") {  
-    message.channel.send(
-      {embed: {color: 0xffff00,fields: [
-        {name: "⚠ 警告処分",value: "当サーバーで軽度のルール違反を犯すと、「警告処分中」の役職が付きます。\n役職がつけられた理由は以下の通りです。\n\n```・チャットやVCでの著しく誹謗な発言\n・VCチャンネルの不正利用（入退室を繰り返すなど）\n・その他、運営が不適切と判断し役職付与が適当と判断した場合```\nこの役職が付与されている者は、次回以降の違反が厳罰化されます。もう一度ルールをご確認頂き、ルール違反の再発防止に努めて下さい。",inline: false},
-        {name: "次回の違反は利用制限/BANになります",value: "2回目以降の違反は厳罰化され、違反行為により利用制限、BAN、Kick等の処罰が科されますので、十分にご注意下さい。",inline: false},
-        {name: "誤審・不服申立て",value: "あなたはこの処罰に対して、誤審・不服を申し立てる権利を有します。この処罰に対して不服・誤審だと思う場合は、当サーバーの管理人 <@532928957402710046> まで**__DM__**でご連絡下さい。追って連絡を返信させて頂きます。",inline: false}
-     ]}});
+  //ping測定
+  if(message.content.match(/サーバー落|ping/) || message.content == "ラグ") {
+    message.channel.send({embed: {color: 0x00ffff,fields: [{name: "⏱ Ping",value: "このメッセージが送信された時のPingは **" + client.ws.ping + "ms** でした。",inline: false},]}});
     return;
   }
-  
-  //処罰
-  if (message.content == "MSG_PUNISH") {  
-    message.channel.send(
-      {embed: {color: 0xff0000,fields: [
-        {name: "🚫 利用制限措置",value: "あなたは、当サーバーで重大な違反、もしくは軽度な違反を二度繰り返したため、このサーバーでの行為が制限されています。\n一定時間経過後には解除されますが、過去の違反や今回の違反により、処罰時間が変動する場合があります。",inline: false},
-        {name: "誤審・不服申立て",value: "あなたはこの処罰に対して、誤審・不服を申し立てる権利を有します。この処罰に対して不服・誤審だと思う場合は、当サーバーの管理人 <@532928957402710046> まで**__DM__**でご連絡下さい。追って連絡を返信させて頂きます。",inline: false}
-      ]}});
+  //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+  //いま何日目？
+  if(message.content.match(/何日目|滞在|いつここ(入|来)/)) {
+    var stay_status = Math.round((Date.now() - message.member.joinedAt) / 86400000);
+    message.channel.send({embed: {color: 0x00ffff,fields: [{name: ":flag_white: サーバー滞在日時を確認",value: "<@" + message.member + "> さんはこのサーバーに入って **" + stay_status + "** 日目です。",inline: false},]}});
+  }
+  //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+  //時間を聞く
+  if(message.content.match(/何時|^なんじ|time/i)) {
+    message.channel.send({embed: {color: 0x00ffff,fields: [{name: "🕑 日付と時刻",value: "現在の世界の日付と時刻はこちらです。\n\n🇯🇵 **日本**\n" + getTime_JPN() + "\n\n🇨🇦 **カナダ**\n" + getTime_CAN() + "\n\n🇺🇸 **アメリカ**\n" + getTime_USA(),inline: false},]}});
     return;
   }
+  //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+  //おみくじ
+  if(message.content.match(/おみくじ|オミクジ|御神籤/)) {  
+    const omikuji_rand = Math.floor(Math.random() * omikuji.length);
+    if(omikuji_rand >= 0 && omikuji_rand <= 2) {
+        let suffix = ["と出ました！","でした","です！"];
+      }
+      else if(omikuji_rand >= 3) {
+        let suffix = ["…","です。","でした"];
+      }
+      message.channel.send({embed: {color: 0xF06292,fields: [{name: ":clipboard: おみくじの結果",value: "<@" + message.member + ">さんの結果は、``" + omikuji[omikuji_rand] + "`` " + suffix[Math.floor(Math.random() * suffix.length)],inline: false}]}});
+      return;
+  }
+  
+  //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+  //　この上まで通常処理
+  //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+  
   
   
 });
