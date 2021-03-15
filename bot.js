422f5e81c079fc6ab9619818af70a89b4030e399
Glitch (pumped-chopper) <none>
2021-03-15 19:29:40 +0000

    ⛽️📠 Checkpoint ./bot.js:20482770/1133

--- bot.js
+++ bot.js
@@ -1,8 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var cnt;
+
 var J_PlayerCount = 0;
 var J_PlayerList = [];
+var J_PlayerJobs = [];
 var J_Jobs = ["村人","🔯 占い師","🐺 人狼"];
 
 //▼▼▼▼▼▼▼▼▼▼▼▼▼ プレイ設定 ▼▼▼▼▼▼▼▼▼▼▼▼
@@ -11,7 +14,7 @@ var J_Jobs = ["村人","🔯 占い師","🐺 人狼"];
   let J_PLAYER_LIMIT = 4;
 
 //デバッグモード
-  let j_debug = 1;
+  let J_debug = 1;
 
 //待ち時間
   const j_wait = 180000;
@@ -31,7 +34,7 @@ client.on('message', async message => {
   
   function J_ready(JoinUser) {
     
-    if(ONENIGHT_LIMIT <= 2 || j_debug >= 2) {
+    if(J_PLAYER_LIMIT <= 2 || J_debug >= 2) {
       message.channel.send({embed: {color: 0xff0000,fields: [{name: "⚠ エラーが発生しました",value: "**ワンナイト人狼**を実行する際に問題が発生しました。恐れ入りますが管理人をお呼び下さい。",inline: false},]}});
       return;
     }
@@ -41,11 +44,36 @@ client.on('message', async message => {
       return;
     }
     
+    J_PlayerCount++;
     J_PlayerList[J_PlayerCount] = JoinUser;
-    message.channel.send({embed: {color: 0xAD1457,fields: [{name: "🐺 ワンナイト人狼： 参加完了",value: "<@" + message.author.id +"> \n参加しました。\nあなたは **プレイヤー" + (J_PlayerCount + 1) + "** です。※覚える必要はありません\n" + "\n**●プレイ方法**\nカテゴリ「プレイ中のゲーム」から「ワンナイト人狼」専用チャンネルをご覧ください。必要に応じて専用VCチャンネルもご利用下さい。",inline: false},]}});
+    message.channel.send({embed: {color: 0xAD1457,fields: [{name: "🐺 ワンナイト人狼： 参加完了",value: "<@" + message.author.id +"> \n参加しました。\nあなたは **プレイヤー" + (J_PlayerCount) + "** です。※覚える必要はありません\n" + "\n**●プレイ方法**\nカテゴリ「プレイ中のゲーム」から「ワンナイト人狼」専用チャンネルをご覧ください。必要に応じて専用VCチャンネルもご利用下さい。",inline: false},]}});
     
+    if(J_PLAYER_LIMIT == J_PlayerCount) {
+      message.channel.send(":small_red_triangle_down: " + J_PLAYER_LIMIT + "人集まりました。これよりゲームを開始します！");
+      setTimeout(J_PLAY_DAY1_DAYTIME(),5000);
+    }
   }
   
+  function J_PLAY_DAY1_DAYTIME() {
+    
+    //役職配布
+    for(cnt = 0; cnt < J_PLAYER_LIMIT; cnt++) {
+      J_PlayerJobs[cnt] = 0;
+    }
+    
+    //占い師選抜
+    J_PlayerList[Math.floor(Math.random() * J_PLAYER_LIMIT)] = 1;
+    
+    let J_JinrohRand = Math.floor(Math.random() * J_PLAYER_LIMIT);
+    
+    while(J_PlayerJobs.indexOf(1) == J_JinrohRand) {
+      J_JinrohRand = Math.floor(Math.random() * J_PLAYER_LIMIT);
+    }
+    
+    J_PlayerJobs[J_JinrohRand] = 2;
+    
+    
+  }
   
 
   
