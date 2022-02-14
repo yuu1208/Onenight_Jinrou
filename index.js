ca38f44a08fbc36f481a27f5bf33c63757250b77
Glitch (pumped-chopper) <none>
2022-02-14 03:38:12 +0000

    🚙⚽️ Checkpoint ./package.json:20482770/1512 ./index.js:20482770/5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Response for Uptime Robot
-const http = require('http')
+const http = require('http');
 http
   .createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' })
